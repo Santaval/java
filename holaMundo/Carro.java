165d048673e7e43406a971b5062429ad1d6c177d
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-07-19 04:48:13 +0100

    classes and atributes

---
+++ holaMundo/Carro.java
@@ -0,0 +1,29 @@
+package holaMundo;
+
+public class Carro {
+    // atributos
+    String marca;
+    String color;
+    int año;
+    String modelo;
+    double velocidad; // m/s
+
+    public Carro(String marcaRecibida, String colorRecibido, int añoRecibido, String modeloRecbido, double velocidadRecibida) {
+        this.marca = marcaRecibida;
+        this.color = colorRecibido;
+        this.año = añoRecibido; 
+        this.modelo = modeloRecbido;
+        this.velocidad = velocidadRecibida;
+
+    }
+
+    public Carro(String marcaRecibida, double velocidad) {
+        this.marca = marcaRecibida;
+        this.velocidad = velocidad;
+    }
+
+    public Carro() {
+        System.out.println("No hago nada");
+    }
+
+}
---
+++ holaMundo/Main.java
@@ -0,0 +1,13 @@
+package holaMundo;
+
+public class Main {
+
+    public static void main(String[] argv) {
+        // tipo de dato - nombre de variable - = - new - contructor
+        Carro carro1 = new Carro("Testla", "morado", 2023, "Model X", 100);
+        Carro carro2 = new Carro("Mercedes", "rojo", 2015, "RT43", 50);
+        Carro carro3 = new Carro("Mitsubichi", 200.0);
+        Carro carro4 = new Carro();
+    }
+
+}
